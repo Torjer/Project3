e883bd0148565def61d6230927e2bf9c16758a03
Caro5290 <anacarogongoram@gmail.com>
2021-02-06 17:30:42 -0600

    Cambié el logo de la app. Será una app para guardar contactos. En total serán 3 activitys, una principal, la de lista de contactos y la de agregar contactos.

--- app/src/main/java/com/example/project3/MainActivity.kt
+++ app/src/main/java/com/example/project3/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.project3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewButton :Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewButton = findViewById(R.id.view_button)
+
+        viewButton.setOnClickListener { _->
+            val intent = Intent(this, ContactView::class.java);
+            startActivity(intent);
+        }
+
+        // Create URL
+
+        // Create URL
+       /* val restdbEndpoint = URL("https://inventory-fac4.restdb.io/rest/motorbikes")
+
+// Create connection
+
+// Create connection
+        val myConnection: HttpsURLConnection = restdbEndpoint.openConnection() as HttpsURLConnection
+
+        myConnection.setRequestProperty("User-Agent", "my-restdb-app")
+        myConnection.setRequestProperty("Accept", "application/json")
+        myConnection.setRequestProperty("x-apikey", "560bd47058e7ab1b2648f4e7")
+
+        if (myConnection.getResponseCode() === 200) {
+            // Success
+            // Further processing here
+        } else {
+            // Error handling code goes here
+        }*/
     }
 }
