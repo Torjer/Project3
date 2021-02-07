1a1655ef9eefb23c27b09d4260afede21efd4da3
IchiroOsalde <ichiroosalde@gmail.com>
2021-02-07 00:31:29 -0600

    Conexión a la base de datos

--- app/src/main/java/com/example/project3/ContactView.kt
+++ app/src/main/java/com/example/project3/ContactView.kt
@@ -1,5 +1,7 @@
 package com.example.project3
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,17 +13,43 @@ class ContactView : AppCompatActivity() {
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    companion object{
+        const val EXTRA_NAME_INFO = "com.example.project3.fullName_info"
+        const val EXTRA_MAIL_INFO = "com.example.project3.mail_info"
+        const val EXTRA_PHONE_INFO = "com.example.project3.phone_info"
+
+        fun createIntent(
+            packageContext: Context, fullNameInfo: Array<String>, mailInfo: Array<String>, phoneInfo: Array<String>): Intent {
+            return Intent(packageContext, ContactView::class.java).apply{
+                putExtra(EXTRA_NAME_INFO, fullNameInfo)
+                putExtra(EXTRA_MAIL_INFO, mailInfo)
+                putExtra(EXTRA_PHONE_INFO, phoneInfo)
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_view)
 
+        val fullName = intent.getStringArrayExtra(EXTRA_NAME_INFO)
+        val mail = intent.getStringArrayExtra(EXTRA_MAIL_INFO)
+        val phone = intent.getStringArrayExtra(EXTRA_PHONE_INFO)
+        var i = 0
+
         val contactsList = mutableListOf<Contact>(
-            Contact("Mariana Lopez", 9991946753, "marlo@correo.com" ),
-            Contact("Esteban Díaz", 9991973227, "ediaz@gmail.com"),
-            Contact("Nina Mendez", 9992347567, "ninamendez@hotmail.com"),
-            Contact("Maximiliano Torres", 9991005533, "maxto@correo.com"),
-            Contact("Olivia Molina", 9991923465, "molivia@gmail.com")
+            //Contact("Mariana Lopez", 9991946753, "marlo@correo.com" )
+            //Contact("Esteban Díaz", 9991973227, "ediaz@gmail.com"),
+            //Contact("Nina Mendez", 9992347567, "ninamendez@hotmail.com"),
+            //Contact("Maximiliano Torres", 9991005533, "maxto@correo.com"),
+            //Contact("Olivia Molina", 9991923465, "molivia@gmail.com")
         )
+
+        phone?.forEach {
+            contactsList.add(Contact(fullName!![i] , it.toLong(), mail!![i]) )
+            i++
+        }
 
 
 
--- app/src/main/java/com/example/project3/MainActivity.kt
+++ app/src/main/java/com/example/project3/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.project3
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.mashape.unirest.http.Unirest
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
+
+private const val CONTACT_ACTIVITY_REQUEST_CODE = 0;
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewButton :Button
+    private lateinit var testText : TextView
+    private lateinit var tempArray : JSONArray
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +27,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewButton = findViewById(R.id.view_button)
+        testText = findViewById(R.id.test)
 
         viewButton.setOnClickListener { _->
-            val intent = Intent(this, ContactView::class.java);
-            startActivity(intent);
+            val size = tempArray.length()
+            val namesList = mutableListOf<String>()
+            val mailsList = mutableListOf<String>()
+            val numbersList = mutableListOf<String>()
+            var i = 0
+            while(i<size){
+                namesList.add(JSONObject(tempArray.getString(i)).getString("fullName"))
+                mailsList.add(JSONObject(tempArray.getString(i)).getString("mail"))
+                numbersList.add(JSONObject(tempArray.getString(i)).getString("number"))
+                i++
+            }
+            startActivityForResult(ContactView.createIntent(this,namesList.toTypedArray(),
+                mailsList.toTypedArray(), numbersList.toTypedArray()), CONTACT_ACTIVITY_REQUEST_CODE)
         }
+
+        doAsync {
+            val response = Unirest.get("https://contactsdb-0225.restdb.io/rest/contacts")
+                .header("x-apikey", "5d915cec62b71c981ecaa0548e9bf18c3c7d8")
+                .header("cache-control", "no-cache").asString()
+            tempArray = JSONArray(response.body)
+             //all = JSONObject(JSONArray(response.body).getString(0)).getString("age")
+        }
+
+//
+        //val allRes = JSONObject(response.body).getString("age")
+
+        //testText.text = allRes.toString()
 
         // Create URL
 
